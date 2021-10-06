7e6036ad90b4650e02e9bb1a6d0cb5230260fb2c
ruturaj-kalal <90278739+ruturaj-kalal@users.noreply.github.com>
2021-10-05 21:51:30 -0400

    fw_wp_landing_page_1010

---
+++ fw-landing-page-template.php
@@ -0,0 +1,368 @@
+<?php
+/*
+* Template Name: FW Landing Page Template
+*/
+
+get_header(); ?>
+
+ 
+<!-- Google Tag Manager (noscript) -->
+	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KBQTM98"
+	height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<body>
+	<div id="event-landing-page">
+		<div class="fw-section-spacing">
+			<div class="first-section-top-right">
+				<div class="top-right-inner">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/fw_images/Rectangle636.png" class="fw-img-responsive" alt="Top Right Image" />
+					<span>XXXXX</span>
+				</div>				
+			</div>
+			<div class="landing-first-section common-section">			
+				<div class="live-streaming-video-wrapper">
+					<div class="live-streaming-video">
+					<div class="fw-wp-story fw-img-responsive">
+						<!-- wp:firework-videos/web-embed
+							{"layout":"storyblock","businessId":"5myE05","channelId":"VxK6zpe","channelValue":"5myE05-VxK6zpe","playlistId":"a_specific_video","entity":"video","autoplay":true,"openMethodValue":"default","onlyShowVideosSelected":true,"mode":"row","channelUsername":"ruturajchannel613f4b70f06f2","videoId":"g46Mle"} -->
+							<fw-storyblock channel="ruturajchannel613f4b70f06f2" video="g46Mle" max_videos="1" autoplay></fw-storyblock>
+						<!-- /wp:firework-videos/web-embed -->
+					</div>
+					</div>
+					<div class="video-title">
+						<div class="text-spacing flex-start-block">
+							<div class="pull-leftside">
+								<h4>Channel</h4>
+								<h2>Makeup 2022 Spring Collection</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="first-section-bottom-left">
+				<div class="bottom-left-inner">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/fw_images/ellipse53.png" class="fw-img-responsive" alt="Bottom Left Image" />
+				</div>
+			</div>
+		</div>
+		<div class="fw-section-spacing">
+			<div class="second-section-top-right">
+				<div class="top-right-inner">
+					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/fw_images/ellipse54.png" class="fw-img-responsive" alt="Second Top Right Image">
+				</div>				
+			</div>
+			<div class="landing-second-section common-section">
+				<div class="live-streaming-video-wrapper section-middle-top-align">			
+					<div class="video-title">
+						<div class="text-spacing flex-end-block">
+							<div class="pull-rightside">
+								<h4>Channel</h4>
+								<h2>Makeup 2023 Spring Collection</h2>
+							</div>
+						</div>					
+					</div>
+					<div class="live-streaming-video">
+						<div class="pull-rightside">
+						<div class="fw-wp-story fw-img-responsive">
+						<!-- wp:firework-videos/web-embed
+							{"layout":"storyblock","businessId":"5myE05","channelId":"VxK6zpe","channelValue":"5myE05-VxK6zpe","playlistId":"a_specific_video","entity":"video","autoplay":true,"openMethodValue":"default","onlyShowVideosSelected":true,"mode":"row","channelUsername":"ruturajchannel613f4b70f06f2","videoId":"g46Mle"} -->
+							<fw-storyblock channel="ruturajchannel613f4b70f06f2" video="g46Mle" max_videos="1" autoplay></fw-storyblock>
+						<!-- /wp:firework-videos/web-embed -->
+						</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="fw-section-spacing">
+			<div class="landing-third-section common-section">
+				<div class="live-streaming-video-wrapper">
+					<div class="live-streaming-video">
+						<div class="third-section-top-left">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/fw_images/ellipse56.png" class="fw-img-responsive" alt="Top Left Image" />
+						</div>
+						<div class="third-section-top-left-2">
+							<img src="<?php echo get_template_directory_uri(); ?>/assets/images/fw_images/rectangle637.png" class="fw-img-responsive" alt="Top Left Overlap Image" />
+						</div>
+						<div class="fw-wp-story fw-img-responsive  z-index-for-img">
+						<!-- wp:firework-videos/web-embed
+							{"layout":"storyblock","businessId":"5myE05","channelId":"VxK6zpe","channelValue":"5myE05-VxK6zpe","playlistId":"a_specific_video","entity":"video","autoplay":true,"openMethodValue":"default","onlyShowVideosSelected":true,"mode":"row","channelUsername":"ruturajchannel613f4b70f06f2","videoId":"g46Mle"} -->
+							<fw-storyblock channel="ruturajchannel613f4b70f06f2" video="g46Mle" max_videos="1" autoplay></fw-storyblock>
+						<!-- /wp:firework-videos/web-embed -->
+						</div>
+					</div>
+					<div class="video-title">
+						<div class="text-spacing flex-start-block">
+							<div class="pull-leftside">
+								<h4>Channel</h4>
+								<h2>Makeup 2022 Spring Collection</h2>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="powered-by-content"><a href="https://firework.tv/"> <img src="<?php echo get_template_directory_uri(); ?>/assets/images/fw_images/fw-logo.png" class="img-power-by-fw" alt="Power by Firework" /></a> </div>
+		</div>
+		<div class="fw-section-spacing">
+			<div class="up-coming-section">
+				<h2 class="up-coming-title">Up coming</h2>
+				<div class="up-coming-section-row">
+					<h3 class="up-coming-subtitle">Jewelry</h3>
+					<div class="story-playlist-carousel">
+						<div class="fw-wp-carousel">
+						<!-- wp:firework-videos/web-embed {"layout":"carousel","businessId":"5myE05","channelId":"VxK6zpe","channelValue":"5myE05-VxK6zpe","playlistId":"","entity":"all_videos_in_channel","autoplay":true,"openMethodValue":"default","mode":"row","channelUsername":"ruturajchannel613f4b70f06f2","videoId":""} -->
+						<fw-embed-feed channel="ruturajchannel613f4b70f06f2" mode="row" autoplay playlist=""></fw-embed-feed>
+						<!-- /wp:firework-videos/web-embed -->
+						</div>
+					</div>
+				</div>
+				<div class="up-coming-section-row">
+					<h3 class="up-coming-subtitle">Skin care</h3>
+					<div class="story-playlist-carousel">
+						<div class="fw-wp-carousel">
+						<!-- wp:firework-videos/web-embed {"layout":"carousel","businessId":"5myE05","channelId":"VxK6zpe","channelValue":"5myE05-VxK6zpe","playlistId":"","entity":"all_videos_in_channel","autoplay":true,"openMethodValue":"default","mode":"row","channelUsername":"ruturajchannel613f4b70f06f2","videoId":""} -->
+						<fw-embed-feed channel="ruturajchannel613f4b70f06f2" mode="row" autoplay playlist=""></fw-embed-feed>
+						<!-- /wp:firework-videos/web-embed -->
+						</div>
+					</div>
+				</div>
+				<div class="up-coming-section-row">
+					<h3 class="up-coming-subtitle">Houseware</h3>
+					<div class="story-playlist-carousel">
+						<div class="fw-wp-carousel">
+						<!-- wp:firework-videos/web-embed {"layout":"carousel","businessId":"5myE05","channelId":"VxK6zpe","channelValue":"5myE05-VxK6zpe","playlistId":"","entity":"all_videos_in_channel","autoplay":true,"openMethodValue":"default","mode":"row","channelUsername":"ruturajchannel613f4b70f06f2","videoId":""} -->
+						<fw-embed-feed channel="ruturajchannel613f4b70f06f2" mode="row" autoplay playlist=""></fw-embed-feed>
+						<!-- /wp:firework-videos/web-embed -->
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	
+ 
+	
+		<?php
+	
+		while ( have_posts() ) : the_post();
+
+		the_content();
+
+		// If comments are open or we have at least one comment, load up the comment template.
+		if ( comments_open() || get_comments_number() ) :
+		comments_template();
+		endif;
+
+		endwhile; // End of the loop.
+		?>        
+	</div>
+	</body>
+<style>
+		#event-landing-page{
+			position: relative;
+			max-width: 1000px;
+			margin: 0 auto;
+		}
+		#event-landing-page .text-spacing h2{
+			font-size: 40px;
+			font-weight: 400;
+		}
+		#event-landing-page .fw-section-spacing{
+			position: relative;
+			padding-left: 80px;
+			padding-right: 80px;
+		}
+		#event-landing-page .pull-rightside{
+			float: right;
+		}
+		#event-landing-page .text-spacing{
+			padding: 30px;
+			display: flex;
+		}
+		#event-landing-page .text-spacing.flex-start-block{
+			justify-content: flex-start;
+		}
+		#event-landing-page .text-spacing.flex-end-block{
+			justify-content: flex-end;
+		}
+		#event-landing-page .common-section{
+			width: 100%;
+			display: block;
+			position: relative;
+		}
+		#event-landing-page .first-section-top-right{
+			position: absolute;
+			top: 0;
+			right: 0;
+		}
+		#event-landing-page .first-section-top-right .top-right-inner{
+			position: relative;
+		}
+		#event-landing-page .first-section-top-right .top-right-inner span{
+			position: absolute;
+			top: 10px;
+			left: 10px;
+			color: #FFFFFF;
+			letter-spacing: 10px;
+			font-weight: bold;
+		}
+		#event-landing-page .first-section-bottom-left{
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			z-index: 1;
+		}
+		#event-landing-page .first-section-bottom-left .bottom-left-inner{
+			position: relative;
+		}
+		#event-landing-page .second-section-top-right{
+			position: absolute;
+			top: -110px;
+    		right: 32px;
+		}
+		#event-landing-page .second-section-top-right .top-right-inner{
+			position: relative;
+		}
+		#event-landing-page .common-section.landing-third-section .live-streaming-video-wrapper .live-streaming-video{
+			position: relative;
+		}
+		#event-landing-page .third-section-top-left{
+			position: absolute;
+			top: 0;
+			right: 0;
+			z-index: 1;
+		}
+		#event-landing-page .third-section-top-left-2{
+			position: absolute;
+			top: -110px;
+			right: 60px;
+			z-index: 2;
+    		opacity: 0.8;
+		}
+		#event-landing-page .z-index-for-img{
+			position: relative;
+			z-index: 2;
+		}
+		#event-landing-page .common-section .live-streaming-video-wrapper{
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+			width: 100%;
+			margin-bottom: 20px;			
+		}
+		#event-landing-page .common-section.landing-first-section{
+			z-index: 2;
+		}
+		#event-landing-page .common-section.landing-first-section .live-streaming-video-wrapper{
+			padding-bottom: 110px;
+		}
+		#event-landing-page .text-spacing h2, #event-landing-page .text-spacing h4{
+			text-transform: uppercase;
+		}
+		#event-landing-page .common-section .live-streaming-video-wrapper .live-streaming-video{
+			width: 50%;
+		}
+		#event-landing-page .common-section .live-streaming-video-wrapper .video-title{
+			width: 50%;
+		}
+		#event-landing-page .common-section.landing-second-section{
+			padding-bottom: 20px;
+		}
+		#event-landing-page .common-section .section-middle-top-align{
+			align-items: start;
+		}
+		#event-landing-page .powered-by-content{
+			text-align: right;
+    		font-size: 18px;
+		}
+		#event-landing-page .story-playlist-carousel{
+		   margin-bottom: 20px;
+		   overflow: hidden;
+		}
+		#event-landing-page ul.playlist-carousel-slider{
+			display: flex;
+			justify-content: space-between;
+			list-style: none;
+			padding: 0;
+			margin: 0;
+			column-gap: 10px;
+		}
+		#event-landing-page .img-power-by-fw{
+			max-height: 18px;
+		}
+
+		@media only screen and (max-width: 767px) {
+			#event-landing-page {
+				width: auto;
+    			overflow: hidden;
+				padding-left: 0px;
+				padding-right: 0px;
+			}
+			#event-landing-page .fw-section-spacing{
+				padding-left: 15px;
+				padding-right: 15px;
+			}
+			#event-landing-page .first-section-top-right {
+				right: -220px;
+			}
+			#event-landing-page .text-spacing h2{
+				font-size: 20px;
+			}
+			#event-landing-page .text-spacing{
+				padding: 10px;
+			}
+			#event-landing-page .first-section-top-right .top-right-inner span{
+				display: none;
+			}
+			#event-landing-page .common-section.landing-first-section .live-streaming-video-wrapper, #event-landing-page .common-section.landing-third-section .live-streaming-video-wrapper{
+				flex-direction: column-reverse;
+			}
+			#event-landing-page .common-section .live-streaming-video-wrapper{
+				flex-direction: column;
+			}
+			#event-landing-page .pull-rightside{
+				float: none;
+			}
+			#event-landing-page .common-section .live-streaming-video-wrapper .video-title, #event-landing-page .common-section .live-streaming-video-wrapper .live-streaming-video{
+				width: 100%;
+				display: contents;
+			}
+			#event-landing-page .common-section .section-middle-top-align{
+				align-items: center;
+			}
+			#event-landing-page .second-section-top-right{
+				top: 30px;
+			}
+			#event-landing-page .third-section-top-left-2{
+				right: -30px;    
+				top: 0;			
+			}
+			#event-landing-page .third-section-top-left {
+				bottom: 0;
+				right: auto;
+			}
+			#event-landing-page .text-spacing.flex-start-block{
+				z-index: 5;
+				max-width: 283px;
+			}
+			#event-landing-page .pull-rightside{
+				max-width: 283px;
+			}
+			#event-landing-page .powered-by-content{
+				text-align: center;
+			}
+		}
+		
+	div#page {
+       background: white;
+	}
+	.fw-wp-story {
+    	width: 283px;
+    	height: 477px;
+	}
+	</style>
+<?php
+get_footer();
+
+
+
